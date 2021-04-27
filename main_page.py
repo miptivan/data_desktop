7e4e3b897e4523fe50c331aad5fb7d0d39c62d93
Rainory <gusev.ea@phystech.edu>
2021-04-27 15:55:14 +0300

    main_page

--- main_page.py
+++ main_page.py
@@ -1,8 +1,43 @@
 import pandas as pd
 import numpy as np
+from datetime import date
+
 
 def main_info(ys):
     dfs = []
     for y in ys:
-        exec(f'analys/df = pd.read_csv("EtsySoldOrderItems{y}.csv")')
+        df = pd.read_csv(f'analysis/EtsySoldOrderItems{y}.csv')
         dfs.append(df)
+    
+    for df in dfs:
+        y = int('20' + df['Sale Date'].iloc[0].split('/')[-1])  # строка с годом таблицы
+        df['y'] = [y]*len(df)
+    
+    cur_year = date.today().year
+    def change(x):
+        if str(x[1]) in x[0]:
+            x[0] = x[0].replace(str(int(x[1])), str(cur_year))
+        if str(x[1] + 1) in x[0]:
+            x[0] = x[0].replace(str(int(x[1] + 1)), str(cur_year + 1))
+        if str(x[1] - 1) in x[0]:
+            x[0] = x[0].replace(str(int(x[1] - 1)), str(cur_year - 1))
+        return x[0]
+
+    for df in dfs:
+        df['Item Name'] = df[['Item Name', 'y']].apply(change, axis=1)
+
+    all_items = dfs[0]  # Объединяем таблицы в одну...
+    for i in range(1, len(dfs)):
+        all_items = all_items.append(dfs[i])
+    
+    first_sale = all_items['Sale Date'].min()
+    all_count = str(all_items['Quantity'].sum())
+    all_sold = str(all_items['Item Total'].sum()) + ' usd'
+
+    active_items = pd.read_csv('analysis/EtsyListingsDownload.csv')
+    active_items['Item Name'] = active_items['TITLE']
+    del active_items['TITLE']
+    active_items = pd.merge(active_items, all_items, on='Item Name', how='left')
+    active_items = active_items.groupby('Item Name')['Item Total'].sum()
+    active_items = pd.DataFrame(data=np.array([active_items.index, active_items.values]).T, columns=['Item Name', 'Item Total'])
+    return first_sale, all_count, all_sold, active_items
