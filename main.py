4a0d8af5a18f44ef490c4f741554b37404d6ed08
Rainory <gusev.ea@phystech.edu>
2021-04-28 12:34:16 +0300

    abc with small bag

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # pyuic5 design.ui -o design.py
 from PyQt5 import QtWidgets
 import pandas as pd
-import design_dev
 import sys
 import info_page
 import main_page
 import items_page
 import ABC_analysis
 import matplotlib.pyplot as plt
-#from graphic_widget import canvas
 import os
 os.system('pyuic5 design_dev.ui -o design_dev.py')
+import design_dev
 
 
 list_dir = os.listdir('analysis')
@@ -52,9 +51,9 @@ class Ui(QtWidgets.QMainWindow, design_dev.Ui_MainWindow):
         self.items_page_but.clicked.connect(self.item_page_loader)
         # page_4
         self.abc_analysis_but.clicked.connect(self.abc_page_loader)
-        self.a_but.clicked.connect()
-        self.b_but.clicked.connect()
-        self.c_but.clicked.connect()
+        self.a_but.clicked.connect(self.set_letter_a)
+        self.b_but.clicked.connect(self.set_letter_b)
+        self.c_but.clicked.connect(self.set_letter_c)
         # page_5
         #self.button_page_5.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_2))
         # page_6
@@ -120,19 +119,40 @@ class Ui(QtWidgets.QMainWindow, design_dev.Ui_MainWindow):
         plt.xticks(range(len(ticklabels)), labels=ticklabels)
 
         self.plot.canvas.axes.clear()
+        self.plot.canvas.figure.clear()
         self.plot.canvas.figure = fig
-        self.plot.canvas.axes = ax # ???????????????????????????????????????????????????????
+        self.plot.canvas.axes = ax
         res = []
         for l in ['A', 'B', 'C']:
             res.append([l, abc_items.loc[l], round(abc[abc['group'] == l]['total_percentage'].sum()*100, 2)])
         set_table(self.tableWidget_4, pd.DataFrame(data=res, columns=['group', 'count', 'percentage of profit']))
         return self.stackedWidget.setCurrentWidget(self.abc_analysis)
     
-    def ABC_subpage_loader(letter):
+    def set_letter_a(self):
+        self.letter = 'A'
+        return self.abc_subpage_loader()
+    
+    def set_letter_b(self):
+        self.letter = 'B'
+        return self.abc_subpage_loader()
+    
+    def set_letter_c(self):
+        self.letter = 'C'
+        return self.abc_subpage_loader()
+
+    def abc_subpage_loader(self):
         global ys
         ABC_analysis.abc(ys)
         abc = pd.read_csv('analysis/ABC_table' + '_'.join([str(y) for y in ys]) + '.csv')
-        abc = abc[abc['group'] == letter]
+        abc = abc[abc['group'] == self.letter]
+        abc = abc[['Item Name', 'Item Total']]
+        summ = abc['Item Total'].values.sum()
+        abc['Group percent'] = abc['Item Total']/summ
+        abc = abc[['Item Name', 'Item Total', 'Group percent']]
+        abc.columns = ['Item', 'Total, usd', 'Group percent']
+        set_table(self.tableWidget_7, abc)
+        self.label_22.setText('Товары группы ' + self.letter)
+        return self.stackedWidget.setCurrentWidget(self.abc_subpage)
         
         
 
